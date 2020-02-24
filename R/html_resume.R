e4339a82a87ca6951ad544c1d5075060156ea8a1
Mickaël Canouil <mickael.canouil@cnrs.fr>
2020-02-24 13:49:57 +0100

    use markdown for doc

--- R/html_resume.R
+++ R/html_resume.R
@@ -1,9 +1,9 @@
 #' Create a resume in HTML
 #'
 #' This output format is based on Min-Zhong Lu's HTML/CSS in the Github repo
-#' \url{https://github.com/mnjul/html-resume}. See
-#' \url{https://pagedown.rbind.io/html-resume/} for an example.
-#' @param ...,css,template,number_sections See \code{\link{html_paged}()}.
+#' [https://github.com/mnjul/html-resume](https://github.com/mnjul/html-resume). See
+#' [https://pagedown.rbind.io/html-resume/](https://pagedown.rbind.io/html-resume/) for an example.
+#' @param ...,css,template,number_sections See `html_paged`.
 #' @return An R Markdown output format.
 #' @export
 html_resume <- function(
@@ -34,7 +34,7 @@ html_resume <- function(
   css2 <- grep("[.]css$", css, value = TRUE, invert = TRUE)
   css <- setdiff(css, css2)
   # pagedown:::check_css(css2)
-  
+
   pandoc_args <- c(
     .pandoc_args,
     pandoc_args,
@@ -56,19 +56,19 @@ html_resume <- function(
       extra_dependencies,
       list(
         htmltools::htmlDependency(
-          name = "font-awesome", 
-          version = "5.1.0", 
-          src = system.file("rmd/h/fontawesome", package = "rmarkdown"), 
+          name = "font-awesome",
+          version = "5.1.0",
+          src = system.file("rmd", "h", "fontawesome", package = "rmarkdown"),
           stylesheet = c("css/all.css", "css/v4-shims.css")
         )
       ),
       list(
         htmltools::htmlDependency(
-          name = "paged", 
-          version = packageVersion("pagedown"), 
-          src = system.file("resources", package = "pagedown", mustWork = TRUE), 
-          script = if (.pagedjs) c("js/config.js", if (.test) "js/paged-latest.js" else c("js/paged.js", "js/hooks.js")), 
-          stylesheet = file.path("css", xfun::with_ext(css2, ".css")), 
+          name = "paged",
+          version = packageVersion("pagedown"),
+          src = system.file("resources", package = "pagedown", mustWork = TRUE),
+          script = if (.pagedjs) c("js/config.js", if (.test) "js/paged-latest.js" else c("js/paged.js", "js/hooks.js")),
+          stylesheet = file.path("css", xfun::with_ext(css2, ".css")),
           all_files = .test
         )
       )
